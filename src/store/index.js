fab5741455865ee56f36734f93fcb4677f8c19d5
younhoso <thdbsgh3443@kakao.com>
2022-12-24 14:06:33 +0900

    Add: [리팩토링] state 속성 적용

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,22 @@ import Vuex from "Vuex";
 
 Vue.use(Vuex);
 
+const storage = {
+  fetch(){
+    const arr = [];
+    if (localStorage.length > 0) {
+      for (let i = 0; i < localStorage.length; i++) {
+        const obj = JSON.parse(localStorage.getItem(localStorage.key(i)));
+        arr.push(obj);
+      }
+    }
+    return arr;
+  }
+}
+
 export const store = new Vuex.Store({
-  //
+  state: {
+    todoItems: storage.fetch(),
+    headerText: 'TODO IT!!'
+  }
 });
